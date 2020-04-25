bb84462f02fc179fa7f437042cb3816f5f1e2ee1
Ara Developer <loma1016@gmail.com>
2020-04-26 00:45:49 +0900

    v1.07
    how to get add

--- assets/js/character.js
+++ assets/js/character.js
@@ -5,6 +5,7 @@ let over = document.getElementById('overlay');
 let soulName = document.getElementById('overSoulName');
 let soulType = document.getElementById('overSoulType');
 let soulDes = document.getElementById('overSoulDes');
+let soulGet = document.getElementById('overSoulGet');
 
 function overOn(e) {
   let soulID = event.target.id;
@@ -20,27 +21,32 @@ function overOn(e) {
       soulName.innerHTML = 'blue soul';
       soulType.innerHTML = 'type : tank';
       soulDes.innerHTML = 'It is a strong-willed but slow-running soul.';
+      soulGet.innerHTML = 'HOW TO GET : Achieve 50000 score';
       break;
     case 'napol':
       soulName.innerHTML = 'napol soul';
       soulType.innerHTML = 'type : control';
-      soulDes.innerHTML =
-        "There's a hidden soul in the program! You can find it on the web!";
+      soulDes.innerHTML = "There's a hidden soul in the program!";
+      soulGet.innerHTML = 'HOW TO GET : You can find it on the web!';
       break;
     case 'green':
       soulName.innerHTML = 'green soul';
       soulType.innerHTML = 'type : balance';
-      soulDes.innerHTML = '그린 문장';
+      soulDes.innerHTML =
+        'This gentle soul has a stronger faith than anyone else.';
+      soulGet.innerHTML = 'HOW TO GET : 700000 COIN';
       break;
     case 'yellow':
       soulName.innerHTML = 'yellow soul';
       soulType.innerHTML = 'type : speed';
       soulDes.innerHTML = 'Right and left, up and down very fast!';
+      soulGet.innerHTML = 'HOW TO GET : Achieve 120000 score';
       break;
     case 'purple':
       soulName.innerHTML = 'purple soul';
       soulType.innerHTML = 'type : joker';
       soulDes.innerHTML = 'Spiders have eight legs.';
+      soulGet.innerHTML = 'HOW TO GET : Achieve 2000000 COIN';
       break;
   }
 
